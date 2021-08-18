b57488f831652e9037ae03cf122bccb17b662ece
Zeynep <1998.ze.ka@gmail.com>
2021-08-18 18:01:10 +0300

    login auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,27 @@
 const User = require("../models/users")
+const jwt = require("jsonwebtoken")
+
+const maxAge = 60*60*24
+const createToken = (id) => {
+    return jwt.sign({id}, "gizli kelime", {expiresIn: maxAge})
+}
 
 const login_get = (req, res) => {
     res.render("login", {title:"Login"})
 }
 
-const login_post = (req, res) =>{}
+const login_post = async (req, res) => {
+    const { username, password } = req.body
+    try{
+        const user = await User.login(username, password)
+        const token = createToken(user._id)
+        res.cookie("jwt", token, {httpOnly: true, maxAge: maxAge*1000})
+        res.redirect("/admin")
+    }
+    catch(e){
+        console.log(e)
+    }
+}
 
 const signup_get = (req, res) =>{
     res.render("signup", {title: "Signup"})
--- models/users.js
+++ models/users.js
@@ -13,6 +13,20 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.login = async function(username, password){
+    const user = await this.findOne({username})
+    if(user){
+        const auth = await bcrypt.compare(password, user.password)
+        if(auth) {
+            return user
+        } else {
+            throw Error("parola hatalı...")
+        }
+    } else {
+        throw Error("kullanıcı yok...")
+    }
+}
+
 userSchema.pre("save", async function(next) {
     const salt = await bcrypt.genSalt()
     this.password = await bcrypt.hash(this.password, salt)
